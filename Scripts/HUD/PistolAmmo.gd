4e0f680ea5e759673207ba2f65f1ac912de1b1b8
Jumpshawt <heplaysvidogames@gmail.com>
2021-01-01 17:27:12 -0800

    Moved pistol ammo variables to infortransfer
    This fixed the weird pistol equip bug as well
    though I think it might have made a new one in the process

--- Scripts/HUD/PistolAmmo.gd
+++ Scripts/HUD/PistolAmmo.gd
@@ -4,9 +4,6 @@ onready var reload_popup = $"../../ReloadPopup"
 onready var infotransfer = $"/root/InfoTransfer"
 onready var pistol_ammo_popup = $".."
 
-var pistol_reloading = false
-var pistol_ammo_loaded = 16
-var pistol_reserve_ammo = 48
 var x = 0
 
 signal no_reload
@@ -18,51 +15,50 @@ func _ready():
 	pistol_ammo_bought.connect("pistol_ammo_bought", self, "handle_ammo_bought")
 
 func handle_ammo_bought():
-	pistol_reserve_ammo += 90
+	InfoTransfer.pistol_reserve_ammo += 90
 	emit_signal("no_ammo", 1)
 
 func _process(delta):
 	if infotransfer.gun_state == "pistol":
-		if pistol_ammo_loaded <= 0:
+		if InfoTransfer.pistol_ammo_loaded <= 0:
 			emit_signal("need_to_reload")
 		pistol_ammo_popup.popup()
-		if Input.is_action_just_pressed("shoot") and pistol_ammo_loaded > 0 and not infotransfer.game_paused:
-			pistol_ammo_loaded -= 1
-		self.set_text(str(pistol_ammo_loaded)+" / "+str(pistol_reserve_ammo))
-		if pistol_ammo_loaded <= 0 and pistol_reserve_ammo > 1 or  not infotransfer.gun_state == "pistol":
+		if Input.is_action_just_pressed("shoot") and InfoTransfer.pistol_ammo_loaded > 0 and not infotransfer.game_paused:
+			InfoTransfer.pistol_ammo_loaded -= 1
+		self.set_text(str(InfoTransfer.pistol_ammo_loaded)+" / "+str(InfoTransfer.pistol_reserve_ammo))
+		if InfoTransfer.pistol_ammo_loaded <= 0 and InfoTransfer.pistol_reserve_ammo > 1 or  not infotransfer.gun_state == "pistol":
 			reload_popup.popup()
 			if Input.is_action_just_pressed("reload"):
 				reload_popup.hide()
-		if pistol_reserve_ammo <= 0 and pistol_ammo_loaded <= 0:
-			emit_signal("no_ammo")
-		if pistol_reserve_ammo <= 0 and pistol_ammo_loaded >= 0:
+
+		if InfoTransfer.pistol_reserve_ammo <= 0 and InfoTransfer.pistol_ammo_loaded >= 0:
 			emit_signal("no_reload", 1)
-		elif pistol_reserve_ammo >= 0:
+		elif InfoTransfer.pistol_reserve_ammo >= 0:
 			emit_signal("no_reload", 0)
 	if not infotransfer.gun_state == "pistol": 
 		pistol_ammo_popup.hide()
-	if pistol_reloading:
+	if InfoTransfer.pistol_reloading:
 		yield(get_tree().create_timer(1), "timeout")
-		pistol_reloading = false
+		InfoTransfer.pistol_reloading = false
 
 func _on_PistolReloadTimer_timeout():
-	pistol_reloading = false
-	if pistol_reserve_ammo >= 16:
-		x = 16 - pistol_ammo_loaded
-		pistol_reserve_ammo -= x 
-		pistol_ammo_loaded = 16
-	elif pistol_reserve_ammo >= 0:
-		if pistol_reserve_ammo + pistol_ammo_loaded <= 15:
-			pistol_ammo_loaded += pistol_reserve_ammo 
-			pistol_reserve_ammo = 0
-		if pistol_reserve_ammo + pistol_ammo_loaded == 16:
-			pistol_reserve_ammo = 0
-			pistol_ammo_loaded = 16
-		elif pistol_reserve_ammo + pistol_ammo_loaded >= 17 and pistol_reserve_ammo <= 16:
-			x = pistol_reserve_ammo + pistol_ammo_loaded - 16
-			pistol_ammo_loaded = 16
-			pistol_reserve_ammo = x 
+	InfoTransfer.pistol_reloading = false
+	if InfoTransfer.pistol_reserve_ammo >= 16:
+		x = 16 - InfoTransfer.pistol_ammo_loaded
+		InfoTransfer.pistol_reserve_ammo -= x 
+		InfoTransfer.pistol_ammo_loaded = 16
+	elif InfoTransfer.pistol_reserve_ammo >= 0:
+		if InfoTransfer.pistol_reserve_ammo + InfoTransfer.pistol_ammo_loaded <= 15:
+			InfoTransfer.pistol_ammo_loaded += InfoTransfer.pistol_reserve_ammo 
+			InfoTransfer.pistol_reserve_ammo = 0
+		if InfoTransfer.pistol_reserve_ammo + InfoTransfer.pistol_ammo_loaded == 16:
+			InfoTransfer.pistol_reserve_ammo = 0
+			InfoTransfer.pistol_ammo_loaded = 16
+		elif InfoTransfer.pistol_reserve_ammo + InfoTransfer.pistol_ammo_loaded >= 17 and InfoTransfer.pistol_reserve_ammo <= 16:
+			x = InfoTransfer.pistol_reserve_ammo + InfoTransfer.pistol_ammo_loaded - 16
+			InfoTransfer.pistol_ammo_loaded = 16
+			InfoTransfer.pistol_reserve_ammo = x 
 
 
 func _on_Player_pistol_ammo():
-	pistol_reserve_ammo += int(rand_range(16, 32))
+	InfoTransfer.pistol_reserve_ammo += int(rand_range(16, 32))
