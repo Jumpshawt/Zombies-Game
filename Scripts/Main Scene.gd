3a9f777425741528ed489d4d29a00481d7ffd8fa
MegaModular <76797049+MegaModular@users.noreply.github.com>
2021-01-05 14:54:08 -0800

    Health System
    WIP

--- Scripts/Main Scene.gd
+++ Scripts/Main Scene.gd
@@ -5,26 +5,13 @@ var a = 1
 
 func _ready():
 	print("start")
-	
+	$"Round Change".play()
+	$"Round Change".set_volume_db(-20)
 
 func _process(_delta):
 	if infotransfer.changing_rounds == true and a == 1:
 		$"Round Change".play()
 		a = 0
 	if a == 0:
-		var t = Timer.new()
-		t.set_wait_time(10)
-		t.set_one_shot(true)
-		self.add_child(t)
-		t.start()
-		yield(t, "timeout")
-		t.queue_free()
+		yield(get_tree().create_timer(10, true), "timeout")
 		a = 1
-
-
-func _on_Enemy2_stop_walking():
-	pass # Replace with function body.
-
-
-func _on_Enemy2_walk():
-	pass # Replace with function body.
