eb816561382564ce92976f14cc29f6774c181566
MegaModular <76797049+MegaModular@users.noreply.github.com>
2021-01-14 14:16:22 -0800

    have se x:)  hi!

--- Scripts/Objects/PistolMachine.gd
+++ Scripts/Objects/PistolMachine.gd
@@ -6,11 +6,28 @@ onready var infotransfer = $"/root/InfoTransfer"
 signal pistol_shop_popup
 
 func _ready():
+	$Control/Label.visible = false
+	$Control/Label2.visible = false
 	var hand_interact = get_tree().get_root().find_node("Player", true, false)
-	hand_interact.connect("interacted", self, "handle_interacted")
+	hand_interact.connect("interactee", self, "handle_interacted")
+	var popup = get_tree().get_root().find_node("Player", true, false)
+	popup.connect("door_popup", self, "handle_interactee")
 
 func handle_interacted(eee):
+	print("signal recieved")
 	if eee == static_body:
-		if infotransfer.gun_state == "shotgun":
-			infotransfer.gun_state == "shotgun_fix"
-		emit_signal("pistol_shop_popup")
+		if infotransfer.money >= 500:
+			infotransfer.money -= 500
+			infotransfer.pistol_reserve_ammo += 120
+		elif infotransfer.money < 500:
+			$Control/Label2.visible = true
+
+func handle_interactee(eee):
+	if eee == $StaticBody:
+		$LabelTimer.stop()
+		$LabelTimer.start()
+		$Control/Label.visible = true
+
+func _on_LabelTimer_timeout():
+	$Control/Label.visible = false
+	$Control/Label2.visible = false
--- Scripts/Objects/RifleMachine.gd
+++ Scripts/Objects/RifleMachine.gd
@@ -9,16 +9,19 @@ func _ready():
 	$Control/Label.visible = false
 	$Control/Label2.visible = false
 	var hand_interact = get_tree().get_root().find_node("Player", true, false)
-	hand_interact.connect("interacted", self, "handle_interacted")
+	hand_interact.connect("interactee", self, "handle_interacted")
 	var popup = get_tree().get_root().find_node("Player", true, false)
 	popup.connect("door_popup", self, "handle_interactee")
 
 func handle_interacted(eee):
+	print("signal recieved")
 	if eee == static_body:
 		if not infotransfer.rifle_activated:
 			if infotransfer.money >= 1500:
 				infotransfer.money -= 1500
 				infotransfer.rifle_activated = true
+				infotransfer.gun_changing = true
+				infotransfer.gun_state = "rifle"
 				$Control/Label.set_text("E to Buy Rifle Ammo \nCost = 500 ")
 			elif infotransfer.money <= 1500:
 				$Control/Label2.visible = true
@@ -28,7 +31,6 @@ func handle_interacted(eee):
 				infotransfer.rifle_reserve_ammo += 120
 			elif infotransfer.money < 500:
 				$Control/Label2.visible = true
-
 
 func handle_interactee(eee):
 	if eee == static_body:
