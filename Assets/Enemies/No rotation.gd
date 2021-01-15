e0a5692db13b648b0c133356f74d7bb3b47b42b5
Jumpshawt <heplaysvidogames@gmail.com>
2021-01-14 21:41:16 -0800

    lights script

---
+++ Assets/Enemies/No rotation.gd
@@ -0,0 +1,20 @@
+extends CollisionShape
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	set_as_toplevel(true)
+	self.transform = get_parent.transform
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
